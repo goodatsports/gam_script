5f72ff92396c04bd09ebbf6b458274f9d97e225d
Jack Chambers <goodatsports@gmail.com>
2018-04-02 15:54:20 -0700

    Filled out menu with option to view user's group membership, delete group membership etc

--- gam_tests.sh
+++ gam_tests.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
 USERPROMPT="Enter username: "
+# Parse Gmail domain from json file
+DOMAIN="$(jq -r '.domain' env.json)"
 while :
 do
     cat<<EOF
     ---------------
     (1) Add new user
-    (2) Get user info
+    (2) Get user's group membership info 
     (3) Add user to group
-    (4) Quit
+    (4) Remove user from all groups
+    (5) Quit
     ---------------
 EOF
-
-    read -n1 -s
+    read -p "> " -n1
+    echo ""
     case "$REPLY" in
     "1") read -p "$USERPROMPT" USR
          read -p "Enter first name: " FIRSTNAME
@@ -19,13 +22,23 @@ EOF
          read -sp "Enter new password: " PASS
          printf "\n"
          gam create user "$USR" firstname "$FIRSTNAME" lastname "$LASTNAME" password "$PASS" 
-         echo "User $USR successfully created" ;;
+         echo "User $USR successfully created"
+         printf "\n" ;;
     
     "2") read -p "$USERPROMPT" USR
-         gam info user "$USR"
-         exit                   ;;
-    "3") echo "$USERPROMPT"     ;;
-    "4") exit                   ;;
-     * ) echo "Invalid option"  ;;
+         gam print groups member "$USR@$DOMAIN" && printf "\n" ;;
+    "3") read -p "$USERPROMPT" USR
+         read -p "Enter group name: " GROUP
+         gam update group $GROUP add member "$USR@$DOMAIN" 
+         printf "\n" ;;
+    "4") read -p "$USERPROMPT" USR
+         read -p "*** WARNING *** This will remove user $USR from ALL $DOMAIN Gmail groups. 
+            Are you sure you want to do this? (y/n):" CONFIRM
+         case "$CONFIRM" in
+         ["Yy"]) gam print groups | gam csv - gam update group ~Email delete "$USR@$DOMAIN" ;;
+             * ) break           ;;
+         esac                    ;;
+    "5") exit                    ;;
+     * )  echo "Invalid option"  ;;
     esac
 done 
